0d5a2f2f70e53787f78d6d5a6dba9cd0999716c9
David Klingenberg <dklingenberg@gmail.com>
2018-04-28 20:26:19 +0200

    chore: Export scopes and configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export * from './au-date-time-picker';
-
+export * from './configuration';
+export * from './scope';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources(PLATFORM.moduleName('./au-date-time-picker'));
